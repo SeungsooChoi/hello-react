d2d080c9f18d381c09f4bcd009b4a3d749a5c7d3
SeungsooChoi <tmdtn5389@gmail.com>
2021-11-24 20:16:32 +0900

    test : 컴포넌트, 이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 function App() {
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: "48px",
-    padding: 16,
-  };
-  return (
-    <>
-      <h1 style={style}>Hello react</h1>
-      <Say />
-    </>
-  );
+  return <EventPractice />;
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // state의 초기값 설정
   state = {
     number: 0,
+    fixNumber: 0,
   };
 
   render() {
-    const { number } = this.state;
+    const { number, fixNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixNumber}</h2>
         <button
           onClick={() => {
             this.setState({ number: number + 1 }, () => {
-              console.log("온클릭");
+              console.log("누름");
             });
           }}
         >
